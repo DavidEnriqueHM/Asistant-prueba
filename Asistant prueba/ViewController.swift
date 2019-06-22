0cb36b079e30dedf8cba1a31e5a5f39d9bf86f70
team cibertec <enriqueodoo@gmail.com>
2019-06-21 18:11:08 -0700

    hice una prueba

---
+++ Asistant prueba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Asistant prueba
+//
+//  Created by Kurosaki Ryuugo on 6/21/19.
+//  Copyright © 2019 Team cibertec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Asistant prueba/clases/userViewController.swift
@@ -0,0 +1,32 @@
+//
+//  userViewController.swift
+//  Asistant prueba
+//
+//  Created by Kurosaki Ryuugo on 6/21/19.
+//  Copyright © 2019 Team cibertec. All rights reserved.
+//
+
+import UIKit
+
+class userViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+ 
+    /*
+     
+     hola como estas  sohy un comentario
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
